d41070ff9f13dd6354b508109df5f91f3e552646
Bruno Martins <bgcngm@gmail.com>
2025-09-03 15:08:28 +0800

    sm8250-common: Declare IMS libs as symlinks during extraction
    Change-Id: Ia1b8097d28dd4888232fe383b830515b1b9bafaa

--- sm8250-common-vendor.mk
+++ sm8250-common-vendor.mk
@@ -629,6 +629,8 @@ PRODUCT_PACKAGES += \
     wfdservice
 
 PRODUCT_PACKAGES += \
+    system_ext_priv-app_ims_lib_arm64_libimscamera_jni_so \
+    system_ext_priv-app_ims_lib_arm64_libimsmedia_jni_so \
     system_ext_priv-app_WfdService_lib_arm64_libwfdnative_so \
     vendor_lib_libEGL_adreno_so \
     vendor_lib_libGLESv2_adreno_so \
