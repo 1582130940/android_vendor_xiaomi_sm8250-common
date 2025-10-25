1bc495ed876151ed64279742875f9f733ac74788
ANUSH MADATHUMKARA <Anush.4376@gmail.com>
2025-10-26 02:00:01 +0800

    sm8250-common: Use QCOM implementation for audio effects
    * AOSP effects doesn't give as thicc'n'sicc bass boost as the QC effects do.

--- sm8250-common-vendor.mk
+++ sm8250-common-vendor.mk
@@ -259,6 +259,9 @@ PRODUCT_PACKAGES += \
     libwfduibcsrcinterface_proprietary \
     libwfdutils_proprietary \
     libasphere \
+    libqcbassboost \
+    libqcreverb \
+    libqcvirt \
     vendor.qti.hardware.wifidisplaysession@1.0_vendor \
     vendor.qti.hardware.wifidisplaysessionl@1.0-halimpl \
     android.hardware.secure_element@1.0-impl \
